7b5f8aaea377e1a9aff9682c418b17023e9cb08b
Virendra Patil <virendrapatil.24vp@gmail.com>
2024-09-09 22:10:45 +0530

    adds wallet adapter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import "./App.css";
 import { TokenLaunchpad } from "./components/TokenLaunchpad";
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 function App() {
-  return <TokenLaunchpad></TokenLaunchpad>;
+  const endpoint =
+    "https://solana-devnet.g.alchemy.com/v2/VyVawzJyVj8hThqvh2XxUIxJ1drDHbb5";
+  const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <div className="wallet-connector">
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+          <TokenLaunchpad />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
 export default App;
